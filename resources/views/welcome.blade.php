cbb0ee7c5e952de34600052f9f83d7c9682ca6c7
Ajrin281 <ajrink221@gmail.com>
2026-06-22 10:20:43 +0600

    green ajrin btn added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,6 +26,9 @@
         <a href="#" class="btn btn-secondary">
             Ajrin
         </a>
+        <a href="#" class="btn btn-success">
+            Ajrin karim
+        </a>
 
     </div>
 
